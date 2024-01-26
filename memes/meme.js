b62e9cd53bf30b00db0713895f1d5cbb5405e2a1
idle <105382091+epicstud@users.noreply.github.com>
2024-01-25 20:41:23 -0500

    Update meme.js

--- memes/meme.js
+++ memes/meme.js
@@ -1,51 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Random Memes Cookie</title>
-    <style>
-        body {
-            margin: 0;
-            overflow: hidden;
-        }
-        #cookie {
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            cursor: pointer;
-        }
-        .meme {
-            position: absolute;
-            width: 50px; /* Adjust the size as needed */
-            height: 50px;
-            animation: floatAndFade 3s linear;
-            pointer-events: none;
-        }
-
-        @keyframes floatAndFade {
-            0% {
-                transform: translate(0, 0);
-                opacity: 1;
-            }
-            50% {
-                transform: translate(25px, -25px);
-                opacity: 0.8;
-            }
-            100% {
-                transform: translate(50px, -50px);
-                opacity: 0;
-            }
-        }
-    </style>
-</head>
-<body>
-    <div id="cookie" onclick="spawnMeme()">
-        <img src="cookie.png" alt="Cookie" width="100" height="100">
-    </div>
-
-    <script>
         function spawnMeme() {
             const memeData = [
                         { src: 'memes/meme1.png', chance: 70 }, // 50% chance
@@ -87,6 +39,3 @@
             // Randomly select a meme from the memeData array
             return memeData[Math.floor(Math.random() * memeData.length)];
         }
-    </script>
-</body>
-</html>
