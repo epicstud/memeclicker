31437caa00c985609671e24dde8c460f7c94b430
Maxwell The Cat <105382091+epicstud@users.noreply.github.com>
2024-01-30 08:28:19 -0500

    Update meme.js

--- memes/meme.js
+++ memes/meme.js
@@ -12,7 +12,7 @@
                 // Add more meme data with chances as needed
             ];
 
-            const cookie = document.getElementById('cookie');
+            const cookie = document.getElementById('noob');
             const cookiePosition = cookie.getBoundingClientRect();
 
             const selectedMeme = getRandomMeme(memeData);
